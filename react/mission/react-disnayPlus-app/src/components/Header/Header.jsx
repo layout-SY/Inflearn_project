52cdbe55d9d5ebe55fa48d817e2b27952637da2c
kdu <boycho2000@naver.com>
2024-11-10 16:48:18 +0900

    feat. Header.js 추가 구현 - Login 시 User의 Google 계정의 이미지로 Login 버튼 변경 - Login된 User 이미지 클릭 시 UserData 함수 추가(해당 함수 미구현)

--- react/mission/react-disnayPlus-app/src/components/Header/Header.jsx
+++ react/mission/react-disnayPlus-app/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CiLogin } from 'react-icons/ci';
 import './header.css';
 import app from '../../api/firebase';
@@ -6,17 +6,25 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
+	const [User, setUser] = useState(null);
+
 	const navigate = useNavigate();
 	const auth = getAuth(app);
 	const provider = new GoogleAuthProvider();
 
+	const googleLogin = async () => {
+		const result = await signInWithPopup(auth, provider);
+		const userCredentials = result.user;
+		setUser(userCredentials);
+		navigate('/main');
+	};
+
 	const handleLogin = () => {
-		signInWithPopup(auth, provider).then((userCredential) => {
-			console.log(userCredential);
-			if (userCredential) {
-				navigate('/main');
-			}
-		});
+		googleLogin();
+	};
+
+	const userDataList = () => {
+		console.log(User);
 	};
 
 	return (
@@ -30,7 +38,11 @@ const Nav = () => {
 
 			{/* <input type="text" placeholder="검색어를 입력하세요" className="search_input" /> */}
 
-			<CiLogin className="login_logo" onClick={handleLogin} />
+			{User ? (
+				<img src={User.photoURL} alt="user image" className="user_image" onClick={userDataList} />
+			) : (
+				<CiLogin className="login_logo" onClick={handleLogin} />
+			)}
 		</nav>
 	);
 };
--- react/mission/react-disnayPlus-app/src/pages/MainPage/MainPage.jsx
+++ react/mission/react-disnayPlus-app/src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from '../../api/axios';
 
 const MainPage = () => {
-	return <div>MainPage</div>;
+	return <div></div>;
 };
 
 export default MainPage;
