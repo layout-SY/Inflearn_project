3d3165943829642e50a32732a78d1cc60f7054f2
kdu <boycho2000@naver.com>
2024-11-10 18:20:43 +0900

    add. 영화 배급사 Company.jsx 추가 docs.  README에 Company 기능 추가

--- react/mission/react-disnayPlus-app/src/pages/MainPage/MainPage.jsx
+++ react/mission/react-disnayPlus-app/src/pages/MainPage/MainPage.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import api from '../../api/axios';
 import requests from '../../api/requests';
 import Banner from '../../components/Banner/Banner';
+import Company from '../../components/Company/Company';
 
 const MainPage = () => {
 	return (
 		<div>
 			<Banner />
+
+			<Company />
 		</div>
 	);
 };
